a537e071778b10a980b04bb79355002531be8649
root <root@localhost.localdomain>
2017-01-02 16:31:02 +0100

    ma calculette

---
+++ Calculette.py
@@ -0,0 +1,41 @@
+################################################################################
+#@Calculatice
+#@02/01/2017
+#@Negouai Darren
+################################################################################
+
+#Module Import
+
+
+
+#Fontions et variables
+
+
+
+#Body du code
+
+print "Bonjour Ordinateur"
+
+entree1 = int(raw_input("Tapez le Premier Chiffre: "))
+entree2 = int(raw_input("Tapez le Second Chiffre: "))
+operateur = raw_input("Tapez l'operateur: ")
+
+if operateur == "+":
+    resultat=entree1+entree2
+print(resultat)
+
+elif operateur =="-":
+  resultat=entree1-entree2
+print(resultat)
+
+elif (operateur == "*")
+resultat=entree1*entree2
+print (resultat)
+
+elif operateur == "/"
+resultat=entree1/entree2
+print (resultat)
+
+
+else:
+print("Merci de choisir entre : +, -, * ou /")
